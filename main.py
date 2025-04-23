4770417450e6930e7c4869c4b9546bbfe57dd7c9
Suhrab <suhrab.k@mail.ru>
2025-04-23 13:58:15 +0400

    Запуск скрипта получения данных

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import json
 from run import run_pipeline
-
+import subprocess
 app = Flask(__name__)
 CORS(app)  # Разрешаем CORS для фронтенда
 
 
 @app.route("/api/run", methods=["GET"])
-def run_data():
-    run_pipeline()
-    return jsonify("Готово!")
+def run_parser():
+    try:
+        # путь до скрипта
+        command = ["xvfb-run", "-a", "python3", "parser.py"]  # или main.py
+
+        # запускаем как подпроцесс
+        result = subprocess.run(command, capture_output=True, text=True, timeout=300)
+
+        if result.returncode == 0:
+            return jsonify({"status": "success", "output": result.stdout})
+        else:
+            return jsonify({"status": "error", "code": result.returncode, "stderr": result.stderr}), 500
+
+    except Exception as e:
+        return jsonify({"status": "failed", "error": str(e)}), 500
 
 
 @app.route("/api/data", methods=["GET"])
